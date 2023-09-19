483dd25bb54971f1814944bac5498e6546e21aa1
isaui <isadestroyed17@gmail.com>
2023-09-19 19:46:25 +0700

    alhamdulillah sudah selesai

--- WaifuCollection/urls.py
+++ WaifuCollection/urls.py
@@ -24,5 +24,3 @@ urlpatterns = [
     path('main/', include('main.urls'))
 
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
