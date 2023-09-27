55e2c96777d2c78be005acc328f23eb876058de8
isaui <isadestroyed17@gmail.com>
2023-09-27 09:20:49 +0700

    selesai

--- main/views.py
+++ main/views.py
@@ -65,9 +65,12 @@ def home(request):
     waifus = Item.objects.filter(user=request.user)
     user = request.user
     card_total = 0
+    last_login = request.COOKIES['last_login']
+    if last_login is None:
+        last_login = "Belum ada data"
     for waifu in waifus:
         card_total += waifu.amount
-    return render(request, "home.html", {'waifus': waifus, 'username': user.username, 'total':card_total, 'last_login': request.COOKIES['last_login'],})
+    return render(request, "home.html", {'waifus': waifus, 'username': user.username, 'total':card_total, 'last_login': last_login,})
 
 
 
